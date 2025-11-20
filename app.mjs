108f0350d0780bd2f6a49b9023ed39ac76ef2bb9
WakaTP-B <courcelles.amaury@orange.fr>
2025-11-20 23:25:59 +0100

    first

--- app.mjs
+++ app.mjs
@@ -2,27 +2,137 @@ import { loadSequelize } from "./database.mjs";
 import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from 'cookie-parser';
 
 /**
  * Point d'entrée de l'application
  * Vous déclarer ici les routes de votre API REST
  */
 async function main() {
-    try {
-        const sequelize = await loadSequelize();
-        const app = express();
+
+    const sequelize = await loadSequelize();
+    const app = express();
+
+    const User = sequelize.models.User;
+    const Post = sequelize.models.Post;
+    const Comment = sequelize.models.Comment;
+
+    const JWT_SECRET = 'TheSecretPublic';
+
+    app.use(express.json());
+    app.use(cookieParser());
 
 
+    // Register
+    app.post('/register', async (req, res) => {
+        const { username, email, password, verifiedPassword } = req.body;
 
+        if (!email || !password || !verifiedPassword) {
+            return res.status(400).json({ message: 'Email, password and verifiedPassword are required' });
+        }
 
+        if (password !== verifiedPassword) {
+            return res.status(400).json({ message: 'Passwords do not match' });
+        }
 
-        app.listen(3000, () => {
-            console.log("Serveur démarré sur http://localhost:3000");
-        });
+        try {
+            const newUser = await User.create({ username, email, password });
+            res.status(201).json({ message: 'User registered successfully', userId: newUser.id, username: newUser.username });
+        } catch (error) {
+            res.status(500).json({ message: 'Error registering user', error: error.message });
+        }
+    });
 
+    // Route LOGIN
+    app.post('/login', async (req, res) => {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+        try {
+            const user = await User.findOne({ where: { email } });
+            const isPasswordMatch = bcrypt.compareSync(password, user.password);
 
-    } catch (error) {
-        console.error("Error de chargement de Sequelize:", error);
+            if (!user || !isPasswordMatch) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
+            res.cookie('token', token, { httpOnly: true });
+            res.json({ message: 'Login successful' });
+        } catch (error) {
+            res.status(500).json({ message: 'Error logging in', error: error.message });
+        }
+    });
+
+    // Route GET ALL Posts and Comments
+
+    app.get("/all", async (req, res) => {
+        try {
+            const posts = await Post.findAll();
+            const comments = await Comment.findAll();
+
+            res.json({ posts, comments });
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).json("Erreur serveur");
+        }
+    });
+
+    // Middleware pour protection des routes
+    app.use(isLoggedInJWT(User));
+
+    // Route Add-Post
+    app.post("/post", async (req, res) => {
+        const newPostData = req.body;
+
+        try {
+            const newPost = await Post.create({
+                title: newPostData.title,
+                content: newPostData.content,
+                UserId: req.user.id
+            });
+            res.json(newPost);
+        } catch (error) {
+            res.status(500).json({ error: "Erreur lors de la création du Post" });
+        }
+    });
+
+    // Route LOGOUT
+    app.get('/logout', (req, res) => {
+        res.clearCookie('token');
+        res.json({ message: 'Logout successful' });
+    });
+
+    function isLoggedInJWT(UserModel) {
+        return async (req, res, next) => {
+            const token = req.cookies.token;
+            if (!token) {
+                return res.status(401).json({ message: 'Unauthorized: No token provided' });
+            }
+            try {
+                const decoded = jwt.verify(token, JWT_SECRET);
+                req.userId = decoded.userId;
+                req.user = await UserModel.findByPk(req.userId);
+
+                if (!req.user) {
+                    return res.status(401).json({ message: 'Unauthorized' });
+                }
+
+                next();
+            } catch (error) {
+                return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+            }
+        }
     }
+
+
+    app.listen(3000, () => {
+        console.log("Serveur démarré sur http://localhost:3000");
+    });
+
+
 }
 main();
--- database.mjs
+++ database.mjs
@@ -1,4 +1,5 @@
 import { Sequelize, DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
 
 /**
  * 
@@ -6,14 +7,100 @@ import { Sequelize, DataTypes } from "sequelize";
  */
 export async function loadSequelize() {
     try {
-        const sequelize = new Sequelize(/* ... */);
-        // ...
+
+        const login = {
+            database: "app-database",
+            username: "root",
+            password: "root"
+        };
+
+        // Connexion à la BDD
+        const sequelize = new Sequelize(login.database, login.username, login.password, {
+            host: '127.0.0.1',
+            port: 3307,
+            dialect: 'mysql'
+        });
+
+
+        // ----  1. Création de tables via les models ---- 
+        // Création des models (tables) -------------//
+
+        const User = sequelize.define("User", {
+            username: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: {
+                type: DataTypes.STRING,
+                set(clearPassword) {
+                    const hashedPassword = bcrypt.hashSync(clearPassword, 10);
+                    this.setDataValue('password', hashedPassword);
+                }
+            }
+        });
+        const Post = sequelize.define("Post", {
+            title: DataTypes.STRING,
+            content: DataTypes.STRING
+        });
+        const Comment = sequelize.define("Comment", {
+            content: DataTypes.STRING
+        });
+
+
+        User.hasMany(Post);
+        Post.belongsTo(User);
+
+        User.hasMany(Comment);
+        Comment.belongsTo(User);
+
+        Post.hasMany(Comment);
+        Comment.belongsTo(Post);
+
+        await sequelize.sync({ force: true });
+        console.log("Connexion à la BDD effectuée")
+
+        // Init data
+        const userOne = await User.create({
+            username: "Amaury",
+            email: "amaury@mail.com",
+            password: "1234"
+        });
+        const userTwo = await User.create({
+            username: "Meiko",
+            email: "meiko@mail.com",
+            password: "1234"
+        });
+
+        const postOne = await userOne.createPost({ title: "Post test 1", content: "Ceci est le Post 1" });
+
+        const postTwo = await userTwo.createPost({ title: "Post test 2", content: "Ceci est le Post 2" });
+
+        await Comment.create({
+            content: "Ceci est un commentaire du Post 1",
+            UserId: userOne.id,
+            PostId: postOne.id
+        });
+
+        await Comment.create({
+            content: "Ceci est un autres commentaire du Post 1",
+            UserId: userTwo.id,
+            PostId: postOne.id
+        });
+
+        await Comment.create({
+            content: "Ceci est un commentaire du Post 2",
+            UserId: userOne.id,
+            PostId: postTwo.id
+        });
+
+        await Comment.create({
+            content: "Ceci est un autres commentaire du Post 2",
+            UserId: userTwo.id,
+            PostId: postTwo.id
+        });
+
         return sequelize;
+
     } catch (error) {
-        console.error(error);
-        throw Error("Échec du chargement de Sequelize");
+        console.log(error);
+        throw new Error("Connexion impossible à la BDD :(");
     }
-
-    // ...
-
 }
